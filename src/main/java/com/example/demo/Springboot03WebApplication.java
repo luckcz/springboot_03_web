1cb62c272cefb6e7f40fd8c6ca7c0413e0068d72
luckcz <1187117910@qq.com>
2018-10-31 09:32:00 +0800

    springboot_web

---
+++ src/main/java/com/example/demo/Springboot03WebApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03WebApplication.class, args);
+	}
+}
